25e25c2c812f80d56c4374ca372523994258983e
sondhg <sondhw@gmail.com>
2024-05-20 00:07:26 +0700

    merged main branch with redo-style-bootstrap woohoo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import labLogo from "../images/logo 2 trans.png";
 
 const Footer = () => {
   return (
@@ -9,7 +8,6 @@ const Footer = () => {
           &copy; Pham Hoang Anh, Dinh Hoang Son and Do Duc Toan - iPAC Lab,{" "}
           {new Date().getFullYear()}
         </p>
-        <img src={labLogo} alt="iPAC Lab logo" width="50"></img>
       </span>
     </footer>
   );
